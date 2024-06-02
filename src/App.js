14f3dbf0c6136b12f8ed9817cbfb39a4db8f0f9f
Marlen272 <159940678+Marlen272@users.noreply.github.com>
2024-06-03 00:27:27 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import PostsPage from "./pages/postsPage/PostsPage";
+
+function App() {
+    return (
+        <div className="App">
+            <PostsPage/>
+        </div>
+    );
+}
+
+export default App;
